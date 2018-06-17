8a22cc6b5781b9eff9b4203fbf5a832e85df4666
chandanakgd <kgdilipchandana@gmail.com>
2018-06-17 17:39:27 +0530

    del

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: false,
-  firebaseConfig :  {
-    apiKey: "AIzaSyCNooW8iOjViOC3lCV1mphKECYf6MkyE2M",
-    authDomain: "test1-446c0.firebaseapp.com",
-    databaseURL: "https://test1-446c0.firebaseio.com",
-    projectId: "test1-446c0",
-    storageBucket: "",
-    messagingSenderId: "754431225232"
-  }
+  production: false
 };
